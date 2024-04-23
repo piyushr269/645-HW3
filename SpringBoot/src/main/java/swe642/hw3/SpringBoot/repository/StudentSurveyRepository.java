a6c1febe89bd90f95d6225beb1cab233297b11ab
piyushr269 <161773814+piyushr269@users.noreply.github.com>
2024-04-22 23:43:05 -0400

    Delete SpringBoot directory

--- SpringBoot/src/main/java/swe642/hw3/SpringBoot/repository/StudentSurveyRepository.java
+++
@@ -1,8 +0,0 @@
-package swe642.hw3.SpringBoot.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import swe642.hw3.SpringBoot.model.StudentSurvey;
-
-
-public interface StudentSurveyRepository extends JpaRepository<StudentSurvey, Integer> {
-}
